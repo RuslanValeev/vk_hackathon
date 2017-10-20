732d093c837dd3d0abe93ebfb4441c61552a2932
Timofey Nazarov <timothy96@rambler.ru>
2017-10-20 22:05:45 +0300

    some improvement to views

--- vkapp/vkapp/people/views.py
+++ vkapp/vkapp/people/views.py
@@ -9,5 +9,8 @@ def get_user_info(request):
     session = vk.Session()
     vk_api=vk.API(session)
     user_id_get = request.GET.get('usr', '')
+    request_params = request.GET
+    viewer_id = request_params.get('viewer_id')
+    group_id = request_params.get('group_id')
     user1 = vk_api.users.get(user_id=user_id_get, fields='last_seen')
     return(HttpResponse(user1))
