291ade2848d630498d194dfa9e37f08779d75a70
Timofey Nazarov <timothy96@rambler.ru>
2017-10-20 20:24:53 +0300

    hello world mode for user api request

--- vkapp/vkapp/people/views.py
+++ vkapp/vkapp/people/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-import vk as api
+from django.shortcuts import HttpResponse
+import vk
 
+def people_index(request):
+    return(HttpResponse("You are at people index"))
 
-
-# Create your views here.
+def get_user_info(request):
+    session = vk.Session()
+    vk_api=vk.API(session)
+    user_id_get = request.GET.get('usr', '')
+    user1 = vk_api.users.get(user_id=user_id_get, fields='last_seen')
+    return(HttpResponse(user1))
