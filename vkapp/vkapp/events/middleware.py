735b1841995e51a60d501a13a3cd958bc5c5ac47
Timofey Nazarov <timothy96@rambler.ru>
2017-10-21 15:55:03 +0300

    trying to get middleawre to work

--- vkapp/vkapp/events/middleware.py
+++ vkapp/vkapp/events/middleware.py
@@ -2,7 +2,7 @@ def headers_middleware(get_response):
 
     def middleware(request):
         response = get_response(request)
-        response['X-Frame-Options'] = "deny"
+        response['X-Frame-Options'] = 'ALLOW-FROM https://api.vk.com/'
         return response
 
     return middleware
