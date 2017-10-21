5445a2396b83f58cc9a58599f9775ae580a431a0
Timofey Nazarov <timothy96@rambler.ru>
2017-10-21 23:30:25 +0300

    models

--- vkapp/vkapp/events/models.py
+++ vkapp/vkapp/events/models.py
@@ -8,8 +8,8 @@ class Event(models.Model):
     title = models.CharField(max_length=32)
     description = models.CharField(max_length=255)
     city_id = models.IntegerField()
-    start_date = models.DateTimeField
-    end_date = models.DateTimeField
+    start_date = models.CharField(max_length=30, null=True)
+    end_date = models.CharField(max_length=30, null=True)
 
     def __str__(self):
         return(self.title)
