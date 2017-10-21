6a632ba3282b288ebc2bc4234f061443f47ca628
Timofey Nazarov <timothy96@rambler.ru>
2017-10-21 05:43:47 +0300

    model user-event created

--- vkapp/vkapp/people/models.py
+++ vkapp/vkapp/people/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class EventUser(models.Model):
+    afisha_event = models.CharField(max_length=16)
+    vk_user = models.IntegerField(max_length=32)
