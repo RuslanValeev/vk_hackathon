bd71567e4006fcb501fa9e3b25e711de6d11e1bf
Timofey Nazarov <timothy96@rambler.ru>
2017-10-21 20:58:56 +0300

    subscribers getter

--- vkapp/vkapp/matching/views.py
+++ vkapp/vkapp/matching/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import HttpResponse
+from django.http import JsonResponse
 from vkapp.matching.models import EventUser
 from vkapp.people.models import Client
 from vkapp.events.models import Event
@@ -8,8 +9,20 @@ def post_subscribe_user_to_event(request):
     event_id = request.POST.get("event_id")
     client = Client.objects.get(vk_id_ref=user_id)
     event = Event.objects.get(afisha_event_ref=event_id)
-    if client and event:
-        EventUser.create(client, event)
+    if client:
+        EventUser.create(client, event_id)
     print(user_id, event_id)
 
     return HttpResponse(200)
+
+def get_subscribers(request):
+    event_id = request.GET.get("event_id")
+    event_ = Event.objects.get(afisha_event_ref=event_id)
+    response = {}
+    users = []
+    user_entities = EventUser.objects.filter(event=event_)
+    for entity in user_entities:
+        users.append(int(entity.client.vk_id_ref))
+    print(user_entities)
+    response['users'] = users
+    return JsonResponse(response)
