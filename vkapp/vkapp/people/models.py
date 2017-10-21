e34e1767623eaf570adb9141c6031c2616aca22f
Timofey Nazarov <timothy96@rambler.ru>
2017-10-21 14:21:03 +0300

    models fix

--- vkapp/vkapp/people/models.py
+++ vkapp/vkapp/people/models.py
@@ -5,7 +5,7 @@ class Client(models.Model):
     vk_id_ref = models.IntegerField()
     name = models.CharField(max_length=32)
     money = models.IntegerField()
-    is_deleted = models.BinaryField(default=False)
+    # is_deleted = models.BinaryField(default=False)
     description = models.CharField(max_length=255)
     avatar_url = models.CharField(max_length=255)
 
