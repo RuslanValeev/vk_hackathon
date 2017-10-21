70ce7d0fb82d39646030cc8c0d96a7e00ad7c3bc
RuslanValeev <postroobotbox@gmail.com>
2017-10-21 19:58:36 +0000

    deploy fixes

--- vkapp/vkapp/settings.py
+++ vkapp/vkapp/settings.py
@@ -143,8 +143,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'vkapp/fullstatic/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'vkapp/static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    # '/var/www/static/',
 ]
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
